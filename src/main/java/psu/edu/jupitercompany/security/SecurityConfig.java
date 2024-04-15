474b0fa1f619f7e1982886f6cd4756c3b4a58c4b
Luke Hanrahan <lhanrahan2@gmail.com>
2024-04-15 08:43:05 -0400

    Stephanie Work

---
+++ src/main/java/psu/edu/jupitercompany/security/SecurityConfig.java
@@ -0,0 +1,64 @@
+package psu.edu.jupitercompany.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	
+	// add support for JDBC ... no more hardcoded users
+		@Bean
+		public UserDetailsManager userDetaislManager(DataSource dataSource) {
+			
+			JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+			
+			//define query to retrieve a user by username
+			jdbcUserDetailsManager.setUsersByUsernameQuery(
+					"SELECT user_id, pw, active FROM members WHERE user_id=?"
+					);
+			
+			//define query to retrieve authorities/roles by username
+			jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+					"SELECT user_id, role FROM roles WHERE user_id=?"
+					);
+			
+			return jdbcUserDetailsManager;
+		}
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    	
+    	http.authorizeHttpRequests(configurer ->
+    	configurer
+    			  .requestMatchers("/list").hasRole("EMPLOYEE")
+    			  .requestMatchers("/showFormForAdd/**").hasRole("MANAGER")
+    			  .requestMatchers("/showFormForUpdate/**").hasRole("MANAGER")
+    			  .requestMatchers("/delete/**").hasRole("ADMIN")
+    			  .anyRequest().authenticated()
+    			)
+    			.formLogin(form ->
+    					form
+    						.loginPage("/login")
+    						.loginProcessingUrl("/authenticateTheUser")
+    						.permitAll()
+    			)
+    			.logout(logout -> logout.permitAll()
+    					/*)
+    			.exceptionHandling(configurer -> 
+    						configurer.accessDeniedPage("/access-denied")*/
+    					
+    					);
+    	
+    	return http.build();
+    }
+
+}
